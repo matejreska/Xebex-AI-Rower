9937013e727ba3649277b29df848d16c0cc0c6ec
matejreska <matej.reska@gymkren.cz>
2026-05-11 15:06:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+
+const CACHE_NAME = 'xebex-ai-v14'; 
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // Tohle smaže starou mezipaměť (v1), když detekuje novou (v2)
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
